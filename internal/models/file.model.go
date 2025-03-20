d1415fe3aa6c7893f11022cf2c25895de4c6c377
blackprince001 <appiahboaduprince@gmail.com>
2025-03-20 23:00:48 +0000

    add json tagging for handler serializers

--- internal/models/file.model.go
+++ internal/models/file.model.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type File struct {
-	ShortLink   string `gorm:"primaryKey"`
-	Filename    string `gorm:"not null"`
-	TimeUpdated time.Time
+	ShortLink   string    `gorm:"primaryKey" json:"short_link"`
+	Filename    string    `gorm:"not null" json:"file_name"`
+	TimeUpdated time.Time `json:"time_updated"`
 }
