90d2d32c4ee218e603ffdd29ed6d2efeea88f127
blackprince001 <appiahboaduprince@gmail.com>
2025-03-28 10:41:33 +0000

    make filenames unique

--- server/models/file.model.go
+++ server/models/file.model.go
@@ -4,6 +4,6 @@ import "time"
 
 type File struct {
 	ShortLink   string    `gorm:"primaryKey" json:"short_link"`
-	Filename    string    `gorm:"not null" json:"file_name"`
+	Filename    string    `gorm:"not null;unique" json:"file_name"`
 	TimeUpdated time.Time `json:"time_updated"`
 }
