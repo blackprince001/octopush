0c7ed1a88e4e77c18e79232313077d6f35cd1fa3
blackprince001 <appiahboaduprince@gmail.com>
2025-04-03 14:42:23 +0000

    add group names for grouping files

--- server/models/file.model.go
+++ server/models/file.model.go
@@ -5,5 +5,6 @@ import "time"
 type File struct {
 	ShortLink   string    `gorm:"primaryKey" json:"short_link"`
 	Filename    string    `gorm:"not null;unique" json:"file_name"`
+	GroupName   string    `gorm:"not null;default:ungrouped" json:"group_name"`
 	TimeUpdated time.Time `json:"time_updated"`
 }
